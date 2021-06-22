358d0ce93502a79feb0bcd8443f2c7421cc9d144
ShuraimHassan <Shuraimhassan94@gmail.com>
2021-06-23 04:19:52 +0500

    Components Updated accordingly mokeup

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,74 +1,142 @@
-import React from 'react'
+import React from "react";
 
 export default function Card() {
   return (
     <>
-<div>
-  <div className="heading text-center font-bold text-2xl m-5 text-gray-100">Full Responsive Video Cards</div>
-  <div className="holder mx-auto w-10/12 grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
-    <div className="each mb-10 m-2 shadow-lg border-gray-800 bg-gray-100 relative">
-      <img className="w-full" src="https://i.ytimg.com/vi/qew27BNl7io/maxresdefault.jpg" alt="" />
-      <div className="badge absolute top-0 right-0 bg-red-500 m-1 text-gray-200 p-1 px-2 text-xs font-bold rounded">Live</div>
-      <div className="info-box text-xs flex p-1 font-semibold text-gray-500 bg-gray-300">
-        <span className="mr-1 p-1 px-2 font-bold">105 Watching</span>
-        <span className="mr-1 p-1 px-2 font-bold border-l border-gray-400">105 Likes</span>
-        <span className="mr-1 p-1 px-2 font-bold border-l border-gray-400">105 Dislikes</span>
+      <div className="container mx-8 grid gap-4 lg:grid-cols-6 md:grid-cols-4">
+        <div className=' border-2 border-separate'>
+          <img
+            src="https://mdbootstrap.com/img/new/standard/city/031.jpg"
+            className="rounded-lg shadow-xl mb-4"
+            alt=""
+          />
+          <div className="text-center">
+            <h5 className="text-xl font-semibold mb-2">Card title</h5>
+            <p className="mb-4">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
+              sequi tenetur, voluptatibus harum consequuntur alias quaerat
+              excepturi temporibus nisi commodi, ex, ratione quae soluta! Saepe
+              alias dolores dolorem assumenda totam?
+            </p>
+            <button
+              className="bg-purple-500 text-white active:bg-purple-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+              type="button"
+            >
+              Button
+            </button>
+          </div>
+        </div>
+        <div>
+          <img
+            src="https://mdbootstrap.com/img/new/standard/city/031.jpg"
+            className="rounded-lg shadow-xl mb-4"
+            alt=""
+          />
+          <div className="text-center">
+            <h5 className="text-xl font-semibold mb-2">Card title</h5>
+            <p className="mb-4">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
+              sequi tenetur, voluptatibus harum consequuntur alias quaerat
+              excepturi temporibus nisi commodi, ex, ratione quae soluta! Saepe
+              alias dolores dolorem assumenda totam?
+            </p>
+            <button
+              className="bg-purple-500 text-white active:bg-purple-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+              type="button"
+            >
+              Button
+            </button>
+          </div>
+        </div>
+        <div>
+          <img
+            src="https://mdbootstrap.com/img/new/standard/city/031.jpg"
+            className="rounded-lg shadow-xl mb-4"
+            alt=""
+          />
+          <div className="text-center">
+            <h5 className="text-xl font-semibold mb-2">Card title</h5>
+            <p className="mb-4">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
+              sequi tenetur, voluptatibus harum consequuntur alias quaerat
+              excepturi temporibus nisi commodi, ex, ratione quae soluta! Saepe
+              alias dolores dolorem assumenda totam?
+            </p>
+            <button
+              className="bg-purple-500 text-white active:bg-purple-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+              type="button"
+            >
+              Button
+            </button>
+          </div>
+        </div>
+        <div>
+          <img
+            src="https://mdbootstrap.com/img/new/standard/city/031.jpg"
+            className="rounded-lg shadow-xl mb-4"
+            alt=""
+          />
+          <div className="text-center">
+            <h5 className="text-xl font-semibold mb-2">Card title</h5>
+            <p className="mb-4">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
+              sequi tenetur, voluptatibus harum consequuntur alias quaerat
+              excepturi temporibus nisi commodi, ex, ratione quae soluta! Saepe
+              alias dolores dolorem assumenda totam?
+            </p>
+            <button
+              className="bg-purple-500 text-white active:bg-purple-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+              type="button"
+            >
+              Button
+            </button>
+          </div>
+        </div>
+        <div>
+          <img
+            src="https://mdbootstrap.com/img/new/standard/city/031.jpg"
+            className="rounded-lg shadow-xl mb-4"
+            alt=""
+          />
+          <div className="text-center">
+            <h5 className="text-xl font-semibold mb-2">Card title</h5>
+            <p className="mb-4">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
+              sequi tenetur, voluptatibus harum consequuntur alias quaerat
+              excepturi temporibus nisi commodi, ex, ratione quae soluta! Saepe
+              alias dolores dolorem assumenda totam?
+            </p>
+            <button
+              className="bg-purple-500 text-white active:bg-purple-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+              type="button"
+            >
+              Button
+            </button>
+          </div>
+        </div>
+        <div>
+          <img
+            src="https://mdbootstrap.com/img/new/standard/city/031.jpg"
+            className="rounded-lg shadow-xl mb-4"
+            alt=""
+          />
+          <div className="text-center">
+            <h5 className="text-xl font-semibold mb-2">Card title</h5>
+            <p className="mb-4">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
+              sequi tenetur, voluptatibus harum consequuntur alias quaerat
+              excepturi temporibus nisi commodi, ex, ratione quae soluta! Saepe
+              alias dolores dolorem assumenda totam?
+            </p>
+            <button
+              className="bg-purple-500 text-white active:bg-purple-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+              type="button"
+            >
+              Button
+            </button>
+          </div>
+        </div>
       </div>
-      <div className="desc p-4 text-gray-800">
-        <a href="https://www.youtube.com/watch?v=dvqT-E74Qlo" target="_new" className="title font-bold block cursor-pointer hover:underline">Pubg Mobile Custom Room (Unlimited)</a>
-        <a href="https://www.youtube.com/user/sam14319" target="_new" className="badge bg-indigo-500 text-blue-100 rounded px-1 text-xs font-bold cursor-pointer">@dynamo_gaming</a>
-        <span className="description text-sm block py-2 border-gray-400 mb-2">lorem ipsum bekhum bukhum !lorem ipsum bekhum bukhum !</span>
-      </div>
-    </div>
-    {/* each */}
-    <div className="each mb-10 m-2 shadow-lg border-gray-800 bg-gray-100 relative">
-      <img className="w-full" src="https://i.ytimg.com/vi/qew27BNl7io/maxresdefault.jpg" alt="" />
-      <div className="badge absolute top-0 right-0 bg-indigo-500 m-1 text-gray-200 p-1 px-2 text-xs font-bold rounded">10:53</div>
-      <div className="info-box text-xs flex p-1 font-semibold text-gray-500 bg-gray-300">
-        <span className="mr-1 p-1 px-2 font-bold">105 views</span>
-        <span className="mr-1 p-1 px-2 font-bold border-l border-gray-400">105 Likes</span>
-        <span className="mr-1 p-1 px-2 font-bold border-l border-gray-400">105 Dislikes</span>
-      </div>
-      <div className="desc p-4 text-gray-800">
-        <a href="https://www.youtube.com/watch?v=dvqT-E74Qlo" target="_new" className="title font-bold block cursor-pointer hover:underline">Pubg Mobile Custom Room (Unlimited)</a>
-        <a href="https://www.youtube.com/user/sam14319" target="_new" className="badge bg-indigo-500 text-blue-100 rounded px-1 text-xs font-bold cursor-pointer">@dynamo_gaming</a>
-        <span className="description text-sm block py-2 border-gray-400 mb-2">lorem ipsum bekhum bukhum !lorem ipsum bekhum bukhum !</span>
-      </div>
-    </div>
-    {/* each */}
-    <div className="each mb-10 m-2 shadow-lg border-gray-800 bg-gray-100 relative">
-      <img className="w-full" src="https://i.ytimg.com/vi/qew27BNl7io/maxresdefault.jpg" alt="" />
-      <div className="badge absolute top-0 right-0 bg-indigo-500 m-1 text-gray-200 p-1 px-2 text-xs font-bold rounded">10:53</div>
-      <div className="info-box text-xs flex p-1 font-semibold text-gray-500 bg-gray-300">
-        <span className="mr-1 p-1 px-2 font-bold">105 views</span>
-        <span className="mr-1 p-1 px-2 font-bold border-l border-gray-400">105 Likes</span>
-        <span className="mr-1 p-1 px-2 font-bold border-l border-gray-400">105 Dislikes</span>
-      </div>
-      <div className="desc p-4 text-gray-800">
-        <a href="https://www.youtube.com/watch?v=dvqT-E74Qlo" target="_new" className="title font-bold block cursor-pointer hover:underline">Pubg Mobile Custom Room (Unlimited)</a>
-        <a href="https://www.youtube.com/user/sam14319" target="_new" className="badge bg-indigo-500 text-blue-100 rounded px-1 text-xs font-bold cursor-pointer">@dynamo_gaming</a>
-        <span className="description text-sm block py-2 border-gray-400 mb-2">lorem ipsum bekhum bukhum !lorem ipsum bekhum bukhum !</span>
-      </div>
-    </div>
-    {/* each */}
-    <div className="each mb-10 m-2 shadow-lg border-gray-800 bg-gray-100 relative">
-      <img className="w-full" src="https://i.ytimg.com/vi/qew27BNl7io/maxresdefault.jpg" alt="" />
-      <div className="badge absolute top-0 right-0 bg-indigo-500 m-1 text-gray-200 p-1 px-2 text-xs font-bold rounded">10:53</div>
-      <div className="info-box text-xs flex p-1 font-semibold text-gray-500 bg-gray-300">
-        <span className="mr-1 p-1 px-2 font-bold">105 views</span>
-        <span className="mr-1 p-1 px-2 font-bold border-l border-gray-400">105 Likes</span>
-        <span className="mr-1 p-1 px-2 font-bold border-l border-gray-400">105 Dislikes</span>
-      </div>
-      <div className="desc p-4 text-gray-800">
-        <a href="https://www.youtube.com/watch?v=dvqT-E74Qlo" target="_new" className="title font-bold block cursor-pointer hover:underline">Pubg Mobile Custom Room (Unlimited)</a>
-        <a href="https://www.youtube.com/user/sam14319" target="_new" className="badge bg-indigo-500 text-blue-100 rounded px-1 text-xs font-bold cursor-pointer">@dynamo_gaming</a>
-        <span className="description text-sm block py-2 border-gray-400 mb-2">lorem ipsum bekhum bukhum !lorem ipsum bekhum bukhum !</span>
-      </div>
-    </div>
-    {/* each */}
-  </div>
-</div>
-
     </>
-  )
+  );
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,18 +2,18 @@
 const HeaderComponent = () => {
   return (
     <>
-      <div className="container mx-auto">
+      <div className="container mx-auto mt-3 mb-5">
         <div className="grid grid-cols-3">
           <div className="px-2 py-4">
             <a href="#">
-              <span className="text-logo-urban text-lg uppercase">Urban</span>
-              <span className="text-logo-app text-lg uppercase opacity-75">
+              <span className="text-logo-urban text-xl uppercase">Urban</span>
+              <span className="text-logo-app text-xl uppercase opacity-75">
                 App
               </span>
             </a>
           </div>
 
-          <div className=" my-1 w-auto">
+          <div className=" my-2 w-auto ">
             <div className="bg-white flex items-center rounded-full shadow-xl">
               <input
                 className="ml-4 px-4 rounded-l-full w-full  text-gray-700 leading-tight focus:outline-none"
@@ -21,22 +21,28 @@ const HeaderComponent = () => {
                 type="search"
                 placeholder="Search"
               />
-              <div className="p-3 ">
-                <i className="fas fa-search text-logo-urban px-6"></i>
+              <div className="px-4 py-2 ">
+                <i className="bg-gray-100 rounded-3xl w-6 h-8 flex justify-center items-center fas fa-search text-logo-urban hover:bg-white px-6"></i>
               </div>
             </div>
           </div>
-          <div>
-            <button class="py-2 px-4 rounded-full focus:outline-none ripple-bg-logo-urban  ">
-              Hover
-            </button>
-            <button
-              class="text-orange-500 bg-transparent border border-solid border-orange-500 hover:bg-orange-500 hover:text-white active:bg-orange-600 font-bold uppercase text-xs px-4 py-2 rounded-full outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-              type="button"
-            >
-              Small
-            </button>
-            <button className="">sign out</button>
+          <div className="ml-28 mt-3">
+            <div className="inline-block mr-5 ">
+              <button
+                type="button"
+                className="focus:outline-none text-yellow-300 text-sm py-1.5 px-5 rounded-full border-4 border-yellow-300 hover:bg-yellow-50"
+              >
+                Post add
+              </button>
+            </div>
+            <div className="inline-block mr-2 ">
+              <button
+                type="button"
+                className="focus:outline-none text-white bg-green-500 text-sm py-1.5 px-5 rounded-full border-4 border-green-500 hover:bg-green-700"
+              >
+                Login
+              </button>
+            </div>
           </div>
         </div>
       </div>
