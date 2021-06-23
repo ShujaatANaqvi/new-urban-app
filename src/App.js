965fd2b0d6335ddb117d1654f4876fa6e864822f
Muhammad Shumail <Shuraimhassan94@gmail.com>
2021-06-23 20:40:09 +0500

    main Components Updated

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import Header from "./Components/Header";
 import MidSection from "./Components/MidSection";
 import Card from "./Components/Card";
-import BUtton from "./Components/test";
 import "./styles/tailwind.css";
 import "./index.css";
 
